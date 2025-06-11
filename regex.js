6150ba632ecd789a19f9584786e9ce1506a70856
Ali Trabelsi <ali.trabelsi@hotmail.co.uk>
2025-06-11 20:44:48 +0100

    Update solution to problem 1

--- regex.js
+++ regex.js
@@ -1,16 +1,8 @@
 function problem1(){
     /*
-        ^ - ensure match starts at beginning
-        [+-]? - matches optional plus or minus / ? is 0 or one times
-        (\d{1,3}(,\d{3})*|\d+) - whole numbers
-            matches numbers with comma seperators 
-            or matches numbers without commas
-        (\.\d+)? - optional decimal part / optional 0 or one times
-        ([eE][+-]?\d+)? - optional scientific notation
-            [eE] matches e or E / [+-]? optional + or - after e/E / \d+ one or more digits
-        $ match goes up to end of input
+        Group solution
     */
-    const regex = /^[+-]?(\d{1,3}(,\d{3})*|\d+)(\.\d+)?([eE][+-]?\d+)?$/;
+    const regex = /^-?\d+(?:,\d+)*(?:\.\d+)*(?:e\d+)*$/;
 
     const testCases = [
         "3.14529",       // match
@@ -21,14 +13,13 @@ function problem1(){
         "720p"           // skip
     ];
 
-    testCases.forEach(str => {
+    for (const str of testCases) {
         const result = regex.test(str) ? "match" : "skip";
         console.log(`${str}: ${result}`);
-    });
+    }
 }
 
-function problem2(){
-    const regex = /1/;
+function problem2() {
 
     const testCases = [
         "415-555-1234",     // 415
@@ -37,8 +28,8 @@ function problem2(){
         "202 555 4567",     // 202
         "4035555678",       // 403
         "1 416 555 9292"    // 416
-    ]
-    
+    ];
+
 }
 
 function problem3(){
@@ -49,4 +40,4 @@ function problem4(){
 
 }
 
-problem1();
+problem2();
